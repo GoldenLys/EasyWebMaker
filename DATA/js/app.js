fe6cefec93993d008ac5ccfbcc3ba1bd121313a6
Purple <rmdu45@gmail.com>
2020-07-28 00:21:46 +0200

    v0.5

--- DATA/js/app.js
+++ DATA/js/app.js
@@ -1,7 +1,6 @@
 const APP = {
     url: window.location.href,
-    name: "Easy Web Maker",
-    version: 0.4,
+    version: 0.5,
     icons: {
         normal: "fas fa-comment-alt",
         alert: "fas fa-comment-alt-exclamation",
@@ -14,6 +13,8 @@ const APP = {
         dots: "fas fa-comment-alt-dots",
     },
 };
+
+var CONFIG = ["", false, false, "", "", "", "", "", ""];
 
 const REPLACERS = {
     "title": ["<span class='title'>", "</span>"],
@@ -33,15 +34,15 @@ const REPLACERS = {
 };
 
 const MESSAGES_REPLACERS = {
-    "message": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.normal + " icon'></i></div>"],
-    "message alert": ["<div class='ui black message 'id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.alert + " icon'></i></div>"],
-    "message smile": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.smile + " icon'></i></div>"],
-    "message plus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.plus + " icon'></i></div>"],
-    "message minus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.minus + " icon'></i></div>"],
-    "message edit": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.edit + " icon'></i></div>"],
-    "message dollar": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.dollar + " icon'></i></div>"],
-    "message text": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.text + " icon'></i></div>"],
-    "message dots": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone'><i class='" + APP.icons.dots + " icon'></i></div>"],
+    "message": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.normal + " icon'></i></div>"],
+    "message alert": ["<div class='ui black message 'id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.alert + " icon'></i></div>"],
+    "message smile": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.smile + " icon'></i></div>"],
+    "message plus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.plus + " icon'></i></div>"],
+    "message minus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.minus + " icon'></i></div>"],
+    "message edit": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.edit + " icon'></i></div>"],
+    "message dollar": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dollar + " icon'></i></div>"],
+    "message text": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.text + " icon'></i></div>"],
+    "message dots": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dots + " icon'></i></div>"],
 };
 
 const CUSTOM_REPLACERS = {
@@ -50,9 +51,30 @@ const CUSTOM_REPLACERS = {
 };
 
 (function () {
-    $("#site-title").html(APP.name);
-    $("#site-version").html("v" + APP.version);
-    document.title = APP.name;
+    jQuery.get('source/config.txt', undefined, function (data) {
+        CONFIG[0] = data.match(/website name: ".+?"/)[0].split("website name: ").join("").split('"').join('');
+        if (data.match(/message icons: yes/)) CONFIG[1] = true;
+        if (data.match(/share button: yes/)) CONFIG[2] = true;
+        CONFIG[3] = data.match(/main color: ".+?"/)[0].split("main color: ").join("").split('"').join('');
+        CONFIG[4] = data.match(/accent color: ".+?"/)[0].split("accent color: ").join("").split('"').join('');
+        CONFIG[5] = data.match(/messages color: ".+?"/)[0].split("messages color: ").join("").split('"').join('');
+        CONFIG[6] = data.match(/background color: ".+?"/)[0].split("background color: ").join("").split('"').join('');
+        CONFIG[7] = data.match(/menu color: ".+?"/)[0].split("menu color: ").join("").split('"').join('');
+        CONFIG[8] = data.match(/text color: ".+?"/)[0].split("text color: ").join("").split('"').join('');
+        CONFIG[9] = data.match(/borders color: ".+?"/)[0].split("borders color: ").join("").split('"').join('');
+
+        document.body.style.setProperty('--main', CONFIG[3]);
+        document.body.style.setProperty('--purple', CONFIG[4]);
+        document.body.style.setProperty('--messages', CONFIG[5]);
+        document.body.style.setProperty('--background', CONFIG[6]);
+        document.body.style.setProperty('--menu', CONFIG[7]);
+        document.body.style.setProperty('--text', CONFIG[8]);
+        document.body.style.setProperty('--borders', CONFIG[9]);
+
+        $("#site-title").html(CONFIG[0]);
+        $("#site-version").html("v" + APP.version);
+        document.title = CONFIG[0];
+    });
     jQuery.get("source/content.txt", undefined, function (data) {
         $("#content").html(CONVERT_TEXT(data));
         $("#content").html(GET_IDs($("#content").html()));
@@ -61,9 +83,12 @@ const CUSTOM_REPLACERS = {
 
 const CONVERT_TEXT = function (data) {
     let CONTENT = data.replace(/(\r\n\r\n|\n\n|\r\r)/gm,"<br>");
+    let ShareEnabled = CONFIG[2] ? "<div class='divider'></div><div id='Share' data-id='[URL]'>Share</div></div>" : "</div>";
+    let IconsEnabled = CONFIG[1] ? "" : "disabled";
+
     for (let REPLACE in MESSAGES_REPLACERS) {
         CONTENT = CONTENT.split("(" + REPLACE + ")").join(MESSAGES_REPLACERS[REPLACE][0])
-        .split("(." + REPLACE + ")").join("<div class='divider'></div><div id='Share' data-id='[URL]'>Share</div></div>");
+        .split("(." + REPLACE + ")").join(ShareEnabled).split("[IconsEnabled]").join(IconsEnabled);
     }
     for (let REPLACE in REPLACERS) {
         CONTENT = CONTENT.split("(" + REPLACE + ")").join(REPLACERS[REPLACE][0]).split("(." + REPLACE + ")").join(REPLACERS[REPLACE][1]);
