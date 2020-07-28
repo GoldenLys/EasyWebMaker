e6542e4b87f36cf4686c2bd30d1ebc8579db40fc
Purple <rmdu45@gmail.com>
2020-07-28 02:19:23 +0200

    v0.5.2

--- DATA/js/app.js
+++ DATA/js/app.js
@@ -14,7 +14,7 @@ const APP = {
     },
 };
 
-var CONFIG = ["", false, false, "", "", "", "", "", ""];
+var CONFIG = ["", false, false, false, ""];
 
 const REPLACERS = {
     "title": ["<span class='title'>", "</span>"],
@@ -34,15 +34,15 @@ const REPLACERS = {
 };
 
 const MESSAGES_REPLACERS = {
-    "message": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.normal + " icon'></i></div>"],
-    "message alert": ["<div class='ui black message 'id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.alert + " icon'></i></div>"],
-    "message smile": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.smile + " icon'></i></div>"],
-    "message plus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.plus + " icon'></i></div>"],
-    "message minus": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.minus + " icon'></i></div>"],
-    "message edit": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.edit + " icon'></i></div>"],
-    "message dollar": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dollar + " icon'></i></div>"],
-    "message text": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.text + " icon'></i></div>"],
-    "message dots": ["<div class='ui black message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dots + " icon'></i></div>"],
+    "message": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.normal + " icon'></i></div>"],
+    "message alert": ["<div class='message 'id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.alert + " icon'></i></div>"],
+    "message smile": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.smile + " icon'></i></div>"],
+    "message plus": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.plus + " icon'></i></div>"],
+    "message minus": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.minus + " icon'></i></div>"],
+    "message edit": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.edit + " icon'></i></div>"],
+    "message dollar": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dollar + " icon'></i></div>"],
+    "message text": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.text + " icon'></i></div>"],
+    "message dots": ["<div class='message' id='[MESSAGEID]'><div class='icone [IconsEnabled]'><i class='" + APP.icons.dots + " icon'></i></div>"],
 };
 
 const CUSTOM_REPLACERS = {
@@ -55,25 +55,13 @@ const CUSTOM_REPLACERS = {
         CONFIG[0] = data.match(/website name: ".+?"/)[0].split("website name: ").join("").split('"').join('');
         if (data.match(/message icons: yes/)) CONFIG[1] = true;
         if (data.match(/share button: yes/)) CONFIG[2] = true;
-        CONFIG[3] = data.match(/main color: ".+?"/)[0].split("main color: ").join("").split('"').join('');
-        CONFIG[4] = data.match(/accent color: ".+?"/)[0].split("accent color: ").join("").split('"').join('');
-        CONFIG[5] = data.match(/messages color: ".+?"/)[0].split("messages color: ").join("").split('"').join('');
-        CONFIG[6] = data.match(/background color: ".+?"/)[0].split("background color: ").join("").split('"').join('');
-        CONFIG[7] = data.match(/menu color: ".+?"/)[0].split("menu color: ").join("").split('"').join('');
-        CONFIG[8] = data.match(/text color: ".+?"/)[0].split("text color: ").join("").split('"').join('');
-        CONFIG[9] = data.match(/borders color: ".+?"/)[0].split("borders color: ").join("").split('"').join('');
-
-        document.body.style.setProperty('--main', CONFIG[3]);
-        document.body.style.setProperty('--purple', CONFIG[4]);
-        document.body.style.setProperty('--messages', CONFIG[5]);
-        document.body.style.setProperty('--background', CONFIG[6]);
-        document.body.style.setProperty('--menu', CONFIG[7]);
-        document.body.style.setProperty('--text', CONFIG[8]);
-        document.body.style.setProperty('--borders', CONFIG[9]);
-
+        if (data.match(/main menu: yes/)) CONFIG[3] = true;
+        CONFIG[4] = data.match(/theme: ".+?"/)[0].split("theme: ").join("").split('"').join('');
+        $("<link/>", {rel: "stylesheet", type: "text/css", href: "themes/" + CONFIG[4] + ".css"}).appendTo("head");
         $("#site-title").html(CONFIG[0]);
         $("#site-version").html("v" + APP.version);
         document.title = CONFIG[0];
+        if(!CONFIG[3]) $(".main-menu").attr("style", "display: none!important");
     });
     jQuery.get("source/content.txt", undefined, function (data) {
         $("#content").html(CONVERT_TEXT(data));
@@ -82,7 +70,7 @@ const CUSTOM_REPLACERS = {
 })();
 
 const CONVERT_TEXT = function (data) {
-    let CONTENT = data.replace(/(\r\n\r\n|\n\n|\r\r)/gm,"<br>");
+    let CONTENT = data.replace(/(\r\n\r\n\r\n|\n\n\n|\r\r\r)/gm,"<div class='invisible divider'></div>").replace(/(\r\n\r\n|\n\n|\r\r)/gm,"<div class='horizontal divider'></div>");
     let ShareEnabled = CONFIG[2] ? "<div class='divider'></div><div id='Share' data-id='[URL]'>Share</div></div>" : "</div>";
     let IconsEnabled = CONFIG[1] ? "" : "disabled";
 
@@ -101,7 +89,7 @@ const CONVERT_TEXT = function (data) {
 
 const GET_IDs = function (data) {
     let CurrentURL = APP.url.substring(APP.url.lastIndexOf('#') + 1);
-    $('div.ui.black.message').each(function (index) {
+    $('div.message').each(function (index) {
         if (data.match(/[MESSAGEID]/)) $(this).attr("id", "message-" + index);
         if( index < 10) $(".main-menu").append("<li><a href='#message-" + index + "'>Article " + (index+1) + "</a></li>");
         $(this).children("#Share").attr("data-id", index);
